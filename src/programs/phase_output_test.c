e935b3ccde6fb18e7e15d51d8e80ec0f5a5d6731
Jonathan Challinger <mr.challinger@gmail.com>
2016-12-20 21:30:20 -0800

    phase_output_test: remove printing stuff

--- src/programs/phase_output_test.c
+++ src/programs/phase_output_test.c
@@ -30,26 +30,5 @@ void program_init(void) {
 
 void program_event_adc_sample(float dt) {
     motor_update_state(dt);
-
-    static uint32_t last_print_ms = 0;
-    uint32_t tnow_ms = millis();
-    if (tnow_ms-last_print_ms > 500) {
-        char buf[200];
-        int n = 0;
-        uint8_t reg;
-        for (reg=1; reg<=4; reg++) {
-            uint16_t reg_val = drv_read_register(reg);
-            n += sprintf(buf+n, "0x%X 0b", reg);
-            int8_t i;
-            for (i=10; i>=0; i--) {
-                buf[n++] = ((reg_val>>i)&1) ? '1' : '0';
-            }
-            buf[n++] = '\n';
-        }
-        buf[n++] = '\n';
-        serial_send_dma(n, buf);
-        last_print_ms = tnow_ms;
-    }
-
     motor_run_commutation(dt);
 }
